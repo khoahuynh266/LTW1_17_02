e0986abf8c0eef6ca8a87be9d83bba4dd5b6cb44
Khanhit97 <31893942+Khanhit97@users.noreply.github.com>
2018-01-06 19:01:51 +0700

    commit

---
+++ doanweb-khanh/danh_sach_theo_loai.php
@@ -0,0 +1,60 @@
+<?php 
+	require_once 'init.php';
+	$id=$_GET['id'];
+	$resultSet = selectTheoLoai($id);
+	$resultSet3 = selectLoaiSanPham();
+	$resultSet4 = selectNhaSanXuat();
+?>
+<?php include 'header.php' ?>
+
+<div id="main">
+		<div id="left">
+			<div class="block">
+				<div class="block_sidebar">
+					<h6 href="">Danh sách sản phẩm theo loại</h6>
+				</div>
+				<?php foreach($resultSet as $row): ?>
+					<ul>
+						<li>
+							<a href="chi_tiet_san_pham.php?id=<?php echo $row['id'] ?>" target="_blank"><img src="<?php echo $row['image'] ?>" alt="TenSanPham" /></a>
+							<h5><?php echo $row['tensp'] ?></h5>
+							<p><?php echo  number_format($row['gia']).' VNĐ<br>' ?></p>
+								<?php if (!$currentUser) : ?>
+									<form action="">
+									<button>THÊM VÀO GIỎ HÀNG</button>
+									</form>
+								<?php else: ?>
+										<a href="add.php?id=<?php echo $row['id']?>">Mua
+							<?php endif ?>
+						</li>
+					</ul>
+				<?php endforeach; ?>
+			</div>
+		</div>
+		<div id="right">
+			<div class="block">
+				<div class="block_sidebar">
+					<h6 href="">Loại sản phẩm</h6>
+				</div>
+				<?php foreach($resultSet3 as $row): ?>
+					<ul>
+						<li>
+							<h5><a href="danh_sach_theo_loai.php?id=<?php echo $row['id'] ?>"><?php echo $row['ten'] ?></a></h5>
+						</li>
+					</ul>
+				<?php endforeach; ?>
+
+				<div class="block_sidebar">
+					<h6 href="">Nhà sản xuất</h6>
+				</div>
+				<?php foreach($resultSet4 as $row): ?>
+					<ul>
+						<li>
+							<h5><a href="danh_sach_theo_loai.php?id=<?php echo $row['id'] ?>"><?php echo $row['ten'] ?></a></h5>
+						</li>
+					</ul>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</div>
+
---
+++ doanweb-khanh/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Web Ban Hang</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
+    <div id="header">
+      <div id="header_margin">
+      <div id="logo">
+          <img src="images/logo.png" alt="Logo">
+      </div>
+      <div id="from_search">
+          <form  method="POST">
+              <input type="text_search" name="text_search" id="text_search"  placeholder="Tìm kiếm"  value=""/>
+              <button  type="submit"></button>
+          </form>
+      </div>
+      </div>
+    </div>
+    <div id="slider_menu">
+        <div id ="slider_menu_margin" >
+          <div id="menu">
+              <ul>
+              
+                <li><a href="index.php" title="">Trang chủ</a></li>
+                <?php if (!$currentUser) : ?>
+                            <li> <a href="register.php">Đăng ký</a></li>
+                <?php endif; ?>
+                <?php if (!$currentUser) : ?>
+                            <li> <a href="login.php">Đăng nhập</a></li>
+                <?php endif; ?>
+                <?php if ($currentUser) : ?>
+                <li><a href="" title="">Trang cá nhân</a></li>
+                <li><a href="" title="">Đổi mật khẩu</a></li>
+                <li><a href="" title="">Đăng xuất</a></li>
+              </ul>
+              <?php endif; ?>
+          </div>
+          <div id="slider">
+          </div>
+        </div>
+    </div>
+  </body>
